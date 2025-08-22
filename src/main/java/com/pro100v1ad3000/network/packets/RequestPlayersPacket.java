e5963a24b34f7adcc103b1df6099c5a6c7e49eb4
pro100v1ad <vlad36144@mail.ru>
2025-08-22 15:54:02 +0300

    Первая версия генерации мира в одиночной игре. ( Скорее всего не работает)

--- src/main/java/com/pro100v1ad3000/network/packets/RequestPlayersPacket.java
+++
@@ -1,6 +0,0 @@
-package main.java.com.pro100v1ad3000.network.packets;
-
-import java.io.Serializable;
-
-public class RequestPlayersPacket implements Serializable {
-}
--- src/main/java/com/pro100v1ad3000/world/serialization/SaveFileManager.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.pro100v1ad3000.world.serialization;
-
-public class SaveFileManager {
-}
